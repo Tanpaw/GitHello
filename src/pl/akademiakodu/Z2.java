7c5092df12bf03b8cc9954735fbd097180f559af
Paweł Tanan <pawel.tanan@gmail.com>
2018-08-22 19:34:08 +0200

    added codility solution

--- src/pl/akademiakodu/Z2.java
+++ src/pl/akademiakodu/Z2.java
@@ -1,7 +1,30 @@
 package pl.akademiakodu;
 
 public class Z2 {
-    public static void main(String[] args) {
+        //A[P] + A[Q] + (Q - P)
+        //(A[P] - P) + (A[Q] +Q)
+        int solution(int[] array) {
+            int maxA = array[0];
+            int maxB = array[0];
+
+            for (int i = 0; i < array.length; i++) {
+                if ((array[i] - i) > maxA) {
+                    maxA = array[i] - i;
+                }
+                if (array[i] + i > maxB) {
+                    maxB = array[i] + i;
+                }
+
+            }
+            return maxA + maxB;
+        }
+
+
+        public static void main(String[] args) {
+            Z2 z2 = new Z2();
+            System.out.println(z2.solution(new int[]{1,3,-3}));
+
+        }
 
     }
-}
+
