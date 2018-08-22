cc8b28556fbb0fd3a1beaa86fe83e7aacd590143
Paweł Tanan <pawel.tanan@gmail.com>
2018-08-22 19:38:38 +0200

    added codility solution

--- src/pl/akademiakodu/Z2.java
+++ src/pl/akademiakodu/Z2.java
@@ -1,30 +1,25 @@
 package pl.akademiakodu;
 
 public class Z2 {
-        //A[P] + A[Q] + (Q - P)
-        //(A[P] - P) + (A[Q] +Q)
-        int solution(int[] array) {
-            int maxA = array[0];
-            int maxB = array[0];
+    //A[P] + A[Q] + (Q - P)
+    //(A[P] - P) + (A[Q] +Q)
+    int solution(int[] array) {
+        int maxA = array[0];
+        int maxB = array[0];
 
-            for (int i = 0; i < array.length; i++) {
-                if ((array[i] - i) > maxA) {
-                    maxA = array[i] - i;
-                }
-                if (array[i] + i > maxB) {
-                    maxB = array[i] + i;
-                }
-
+        for (int i = 0; i < array.length; i++) {
+            if ((array[i] - i) > maxA) {
+                maxA = array[i] - i;
             }
-            return maxA + maxB;
+            if (array[i] + i > maxB) {
+                maxB = array[i] + i;
+            }
         }
-
-
-        public static void main(String[] args) {
-            Z2 z2 = new Z2();
-            System.out.println(z2.solution(new int[]{1,3,-3}));
-
-        }
-
+        return maxA + maxB;
     }
+    public static void main(String[] args) {
+        Z2 z2 = new Z2();
+        System.out.println(z2.solution(new int[]{1, 3, -3}));
+    }
+}
 
